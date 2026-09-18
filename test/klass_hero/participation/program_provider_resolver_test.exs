b3599e961a6b4c31cd7145044cf38dffc744be51
MaxPayne89 <maxpergl@gmail.com>
2026-09-18 11:56:03 +0200

    refactor: cut down on BS

--- test/klass_hero/participation/program_provider_resolver_test.exs
+++ test/klass_hero/participation/program_provider_resolver_test.exs
@@ -2,11 +2,12 @@ defmodule KlassHero.Participation.ProgramProviderResolverTest do
   use KlassHero.DataCase, async: true
 
   alias KlassHero.Participation.ProgramProviderResolver
+  alias KlassHero.Factory
 
   describe "resolve_provider_id/1" do
     test "returns provider_id for an existing program" do
-      provider = KlassHero.Factory.insert(:provider_profile_schema)
-      program = KlassHero.Factory.insert(:program_schema, provider_id: provider.id)
+      provider = Factory.insert(:provider_profile_schema)
+      program = Factory.insert(:program_schema, provider_id: provider.id)
 
       assert {:ok, provider_id} = ProgramProviderResolver.resolve_provider_id(program.id)
       assert provider_id == provider.id
@@ -22,8 +23,8 @@ defmodule KlassHero.Participation.ProgramProviderResolverTest do
 
   describe "resolve_provider_details/1" do
     test "returns provider_id and program_title for an existing program" do
-      provider = KlassHero.Factory.insert(:provider_profile_schema)
-      program = KlassHero.Factory.insert(:program_schema, provider_id: provider.id)
+      provider = Factory.insert(:provider_profile_schema)
+      program = Factory.insert(:program_schema, provider_id: provider.id)
 
       assert {:ok, details} = ProgramProviderResolver.resolve_provider_details(program.id)
       assert details.provider_id == provider.id
